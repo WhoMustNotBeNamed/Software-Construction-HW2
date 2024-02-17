8ef2c9921f0eccfdd74eecff2a4212ca5124a977
Alex <yevsyukovaleks@gmail.com>
2024-02-17 19:16:33 +0300

    Spring Boot Initiatizr

---
+++ src/main/java/org/hse/software/construction/HW2/Hw2Application.java
@@ -0,0 +1,13 @@
+package org.hse.software.construction.HW2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/hse/software/construction/HW2/Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hse.software.construction.HW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
